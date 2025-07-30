c7a93bacd3c49ac2d873b4ca361f3ccfc97bb3a6
SinnedHeart <irrealco@gmail.com>
2025-07-29 19:53:44 -0600

    Mistakes we're made.
    Feat: Deploy static site generator to GitHub Pages
    
    - Fixed directory structure to build into docs/ instead of public/
    - Implemented basepath configuration with sys.argv for GitHub Pages
    - Added href and src path replacement for subdirectory deployment
    - Created build.sh script for production builds
    - Cleaned up Git history after deployment configuration mistakes

--- src/generate_content.py
+++ src/generate_content.py
@@ -1,17 +1,17 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         html_filename = filename.replace(".md", ".html")
         dest_path = os.path.join(dest_dir_path, html_filename)
         if os.path.isfile(from_path):
-            generate_page(from_path, template_path,dest_path)
+            generate_page(from_path, template_path,dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"*Generating page {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -28,6 +28,10 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    replace_for_href = f'href="{basepath}'
+    template = template.replace('href="/',replace_for_href)
+    replace_for_src = f'src="{basepath}'
+    template = template.replace('src="/',replace_for_src)
 
     dest_dir_path = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir_path):
--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import os
 import shutil
-from textnode import TextNode, TextType
 from copy_static import copy_files
 from generate_content import generate_pages_recursive
+import sys
 
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_static = "./static"
 from_path = "./content"
 template_path = "template.html"
-dest_path = "./public"
+default_basepath = "/"
+
 
 def main():
+    base_path = default_basepath
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_files(dir_path_static, dir_path_public)
-    generate_pages_recursive(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, dir_path_public, base_path)
 
 if __name__ == "__main__":
     main()
